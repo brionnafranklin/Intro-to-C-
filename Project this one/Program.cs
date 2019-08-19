8c13cedde8ba01f7d6b7c997c942e4aaaa4f4cba
Brionna Franklin <s198017@students.aie.edu>
2019-08-19 15:00:18 -0500

    Add project files.

---
+++ Project this one/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_this_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+            String playerName;
+            int playerScore = 0;
+
+            playerName = Console.ReadLine();
+
+            int add = 0;
+
+            while (playerScore < 10)
+                {
+                //request input
+                Console.Write("Increament Score ");
+                add = Convert.ToInt32(Console.ReadLine());
+                //add score
+                playerScore = playerScore + add;
+                Console.Write("Current Score: ");
+                Console.WriteLine(playerScore);
+
+            }
+
+            Console.Write("Score of ");
+            Console.WriteLine(playerName);
+            Console.WriteLine(playerScore);
+
+            Console.ReadLine();
+        }
+
+    }
+}
